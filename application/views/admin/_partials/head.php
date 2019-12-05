fad3e63c8632b343f41df9d0704b8bd1bd4e2f3f
satriohw <satriohw@users.noreply.github.com>
2019-12-05 20:55:09 +0700

    Perbaikan tampilan sidebar dan navigasi atas
    - head ditambah bagian css untuk dataTable

--- application/views/admin/_partials/head.php
+++ application/views/admin/_partials/head.php
@@ -12,3 +12,4 @@
 
 <!-- Custom styles for this template-->
 <link href="<?php echo base_url('assets/css/sb-admin-2.min.css') ?>" rel="stylesheet">
+<link href="<?php echo base_url('assets/vendor/datatables/dataTables.bootstrap4.min.css') ?>" rel="stylesheet">
