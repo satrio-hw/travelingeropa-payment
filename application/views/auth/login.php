b2413778ac05494a17b32145dc05c2933232b420
jelitacheyra <jelitapanda6@gmail.com>
2019-12-13 18:52:11 +0700

    login
    ini tinggal yg forget password

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -27,60 +27,38 @@
         <!-- Outer Row -->
         <div class="row justify-content-center">
 
-            <div class="col-xl-10 col-lg-12 col-md-9">
+            <div class="col-lg-7">
 
                 <div class="card o-hidden border-0 shadow-lg my-5">
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
-                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-                            <div class="col-lg-6">
+                            <div class="col-lg-6"></div>
                                 <div class="p-5">
-                                    <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
-                                    </div>
-                                    <form class="user">
-                                        <div class="form-group">
-                                            <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
-                                        </div>
-                                        <div class="form-group">
-                                            <div class="custom-control custom-checkbox small">
-                                                <input type="checkbox" class="custom-control-input" id="customCheck">
-                                                <label class="custom-control-label" for="customCheck">Remember Me</label>
-                                            </div>
-                                        </div>
-                                        <a href="index.html" class="btn btn-primary btn-user btn-block">
-                                            Login
-                                        </a>
-                                        <hr>
-                                        <a href="index.html" class="btn btn-google btn-user btn-block">
-                                            <i class="fab fa-google fa-fw"></i> Login with Google
-                                        </a>
-                                        <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                                            <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                                        </a>
-                                    </form>
-                                    <hr>
-                                    <div class="text-center">
-                                        <a class="small" href="forgot-password.html">Forgot Password?</a>
-                                    </div>
-                                    <div class="text-center">
-                                        <a class="small" href="register.html">Create an Account!</a>
-                                    </div>
-                                </div>
-                            </div>
+   
+		<div class="wrap_view">
+		<h1 class="heading text-center text-uppercase mb-5"> Login Admin Traveling Eropa </h1>
+			<div class="inner_sec">
+		
+        <div class="row">
+            <div class="col-12 col-md-5 mx-auto mt-5">
+                <form action="<?= site_url('admin/login') ?>" method="POST">
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="text" class="form-control" name="email" placeholder="Enter Your Email" required />
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" class="form-control" name="password" placeholder="Password" required />
+                    </div>
+                            <a href="<?= site_url('reset_password') ?>">Lupa Password?</a>
                         </div>
                     </div>
-                </div>
+                    <div class="form-group">
+                        <input type="submit" class="btn btn-success w-100" value="Login" />
+                    </div>
 
-            </div>
-
-        </div>
-
-    </div>
+                </form>
 
     <!-- Bootstrap core JavaScript-->
     <script src="<?= base_url("assets/"); ?>vendor/jquery/jquery.min.js"></script>
